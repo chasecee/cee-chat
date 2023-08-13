f0e0fe704533d2f69ff1012cfaca752717128e28
chasechristensen1 <chase.christensen@inmoment.com>
2023-08-13 10:10:44 -0600

    init cee chat

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaComment } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full mt-5 border-b-2 pb-7 sm:px-4 px-2">
-      <Link href="/" className="flex space-x-3">
-        <Image
-          alt="header text"
-          src="/writingIcon.png"
-          className="sm:w-12 sm:h-12 w-8 h-8"
-          width={32}
-          height={32}
-        />
-        <h1 className="sm:text-4xl text-2xl font-bold ml-2 tracking-tight">
-          twitterbio.io
+    <header className="flex justify-between items-center w-full mt-5 border-b pb-7 sm:px-4 px-2">
+      <Link href="/" className="flex gap-3 items-baseline">
+        <FaComment className="text-[1.6rem]" />
+        <h1 className="sm:text-4xl text-2xl font-bold tracking-tight">
+          Cee Chat
         </h1>
       </Link>
       <a
